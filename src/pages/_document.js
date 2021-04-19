12f368c2824feaf7ce64619682915a988568c246
Harsimran <harsimransinghbarki@gmail.com>
2021-04-19 19:40:00 +0530

    added stripe chakra and ecommerce

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,13 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>Rapid Store</title>
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
@@ -13,6 +15,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
